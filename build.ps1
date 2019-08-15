3be2472cf6b63129c6807c7f1b0a4e974f74abab
Flynn Xu <Flynn.Xu@hyland.com>
2019-08-14 23:36:15 -0400

    move R package installation to appveyor.yml from build.ps1

--- build.ps1
+++ build.ps1
@@ -1,8 +1,8 @@
 Write-Host "EBook creation starts"
 
-& "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "install.packages('rmarkdown', repos='http://cran.case.edu/')"
-& "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "install.packages('bookdown', repos='http://cran.case.edu/')"
-& "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "install.packages('reticulate', repos='http://cran.case.edu/')"
+#& "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "install.packages('rmarkdown', repos='http://cran.case.edu/')"
+#& "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "install.packages('bookdown', repos='http://cran.case.edu/')"
+#& "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "install.packages('reticulate', repos='http://cran.case.edu/')"
 
 & "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 
