573332c280de98a9c32c3533079984078877a83d
Flynn Xu <Flynn.Xu@hyland.com>
2019-08-14 23:56:45 -0400

    add git logic

--- build.ps1
+++ build.ps1
@@ -1,9 +1,12 @@
 Write-Host "EBook creation starts"
 
-#& "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "install.packages('rmarkdown', repos='http://cran.case.edu/')"
-#& "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "install.packages('bookdown', repos='http://cran.case.edu/')"
-#& "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "install.packages('reticulate', repos='http://cran.case.edu/')"
+& "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "install.packages('rmarkdown', repos='http://cran.case.edu/')"
+& "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "install.packages('bookdown', repos='http://cran.case.edu/')"
+& "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "install.packages('reticulate', repos='http://cran.case.edu/')"
 
 & "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+& git add docs\*
+& git commit -m "publish new book"
+& git push origin head:master
 
 Write-Host "EBook created!"
