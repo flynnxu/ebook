1aa258cf6316aab50474f6e9084b02f35f089fa0
Flynn Xu <Flynn.Xu@hyland.com>
2019-08-15 00:05:07 -0400

    add git logic in appveyor.yml

--- build.ps1
+++ build.ps1
@@ -5,8 +5,9 @@ Write-Host "EBook creation starts"
 & "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "install.packages('reticulate', repos='http://cran.case.edu/')"
 
 & "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-& git add docs\*
-& git commit -m "publish new book"
-& git push origin head:master
+
+#& git add docs\*
+#& git commit -m "publish new book"
+#& git push origin head:master
 
 Write-Host "EBook created!"
