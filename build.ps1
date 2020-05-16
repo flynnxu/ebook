78aa59b3a3193e5c3dd137eb810ce9f55f1a0ad7
flynnxu <31682166+flynnxu@users.noreply.github.com>
2020-05-16 12:04:11 -0400

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,10 +1,10 @@
 Write-Host "EBook creation starts"
 
-& "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "install.packages('rmarkdown', repos='http://cran.case.edu/')"
-& "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "install.packages('bookdown', repos='http://cran.case.edu/')"
-& "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "install.packages('reticulate', repos='http://cran.case.edu/')"
+& "C:\Program Files\R\R-4.0.0\bin\rscript.exe" -e "install.packages('rmarkdown', repos='http://cran.case.edu/')"
+& "C:\Program Files\R\R-4.0.0\bin\rscript.exe" -e "install.packages('bookdown', repos='http://cran.case.edu/')"
+& "C:\Program Files\R\R-4.0.0\bin\rscript.exe" -e "install.packages('reticulate', repos='http://cran.case.edu/')"
 
-& "C:\Program Files\R\R-3.6.1\bin\rscript.exe" -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+& "C:\Program Files\R\R-4.0.0\bin\rscript.exe" -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 
 #& git add docs\*
 #& git commit -m "publish new book"
