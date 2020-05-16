2f151cdcfb5864dd80e39a4c5c5a13b12ed67877
flynnxu <31682166+flynnxu@users.noreply.github.com>
2020-05-16 12:54:56 -0400

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -4,7 +4,7 @@ Write-Host "EBook creation starts"
 #& "C:\Program Files\R\R-4.0.0\bin\rscript.exe" -e "install.packages('bookdown', repos='https://cran.case.edu/')"
 #& "C:\Program Files\R\R-4.0.0\bin\rscript.exe" -e "install.packages('reticulate', repos='https://cran.case.edu/')"
 
-& "C:\Program Files\R\R-4.0.0\bin\rscript.exe" -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+& "C:\Program Files\R\R-4.0.0\bin\rscript.exe -e \"bookdown::render_book('index.Rmd', 'bookdown::gitbook')\""
 
 #& git add docs\*
 #& git commit -m "publish new book"
